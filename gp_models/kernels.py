f726dbeeaf85090a86370afed930ab99ec8c43d7
Ian Delbridge <iad35@cornell.edu>
2019-05-17 18:24:59 -0400

    better implementation of inverse multiquadratic that doesn't use multiple parameters pointlessly

--- gp_models/kernels.py
+++ gp_models/kernels.py
@@ -526,14 +526,19 @@ class ManualRescaleProjectionKernel(gpytorch.kernels.Kernel):
 
         return self.base_kernel(x1, x2, **params)
 
+
+def postprocess_inverse_mq(dist):
+    return dist.add_(1).pow_(-1/2)
+
+
 class InverseMQKernel(gpytorch.kernels.Kernel):
-    def __init__(self, gamma, **kwargs):
+    def __init__(self, **kwargs):
         super(InverseMQKernel, self).__init__(has_lengthscale=True, **kwargs)
-        self.register_parameter('gamma', torch.nn.Parameter(torch.as_tensor(gamma)))
+        # self.register_parameter('gamma', torch.nn.Parameter(torch.as_tensor(gamma)))
 
     def forward(self, x1, x2, diag=False, last_dim_is_batch=False, **params):
         x1_ = x1.div(self.lengthscale)
         x2_ = x2.div(self.lengthscale)
-        dist = self.covar_dist(x1_, x2_, diag=diag, last_dim_is_batch=last_dim_is_batch, square_dist=True,
-                               postprocess=False)
-        return dist.add_(self.gamma**2).pow_(-1/2).mul_(self.gamma**2)
+        return self.covar_dist(x1_, x2_, diag=diag, last_dim_is_batch=last_dim_is_batch, square_dist=True,
+                               postprocess=True, dist_postprocess_func=postprocess_inverse_mq)
+        # return dist.add_(self.gamma**2).pow_(-1/2).mul_(self.gamma**2)
